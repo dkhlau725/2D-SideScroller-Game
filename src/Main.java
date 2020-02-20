bfaf3ae23d191964f4998c31e0a8d234fbe8d946
dkhlau725 <60667307+dkhlau725@users.noreply.github.com>
2020-02-19 17:08:35 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,41 @@ public class Main {
 
 
     public static void main(String args[]){
-        Player tuan = new Player("Tuan");
+        Player tuan = new Player("Razeen");
+        Bullet tuanBullet = new Bullet("bullet");
 
         Scanner input = new Scanner(System.in);
+        Scanner bulletIn = new Scanner(System.in);
         String command = "";
+        String bcommand = "";
 
         tuan.setPosition(0,600);
+        tuanBullet.setBullet(tuan.getX() + 5, tuan.getY() - 5);
         
 
-        System.out.println("Please enter left, right, or up");
-        
-
+        System.out.println("Please enter left, right or up");
         command = input.nextLine();
-
-
         tuan.move(command);
-
         System.out.println(tuan);
 
 
-        while (command != ""){
-        
-            System.out.println("Please enter left, right, or up");
-        
-
+        while (command != ""){        
+            System.out.println("Please enter left, right, or up");  
             command = input.nextLine();
-
-
             tuan.move(command);
-
             System.out.println(tuan);
-
+            tuanBullet.setBullet(tuan.getX() + 5, tuan.getY() - 5);
+            
+            System.out.println("please enter shoot to shoot");
+            bcommand = input.nextLine();
+            tuanBullet.shoot(bcommand, tuan.getX()+5, tuan.getY()-5);
+			System.out.println("bullet position is " + tuanBullet.getBulletList() );
         }
         input.close();
-    }
+        
 
-
+        }
+    
 }
+
+
