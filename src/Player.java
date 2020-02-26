067e5a093687758e1fa8965736cec6e919db8f93
Razeenadil <60667310+Razeenadil@users.noreply.github.com>
2020-02-26 13:49:46 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,44 +1,57 @@
 public class Player{
+    // decalring private varabiles
     private int x;
     private int y;
     private String name;
     private String input;
    
-
+    //constructor
     public Player(String name){
         this.name = name;
     }
 
+    //setting x and y position
     public void setPosition(int x, int y){
         this.x = x;
         this.y = y;    
 
     }
+	
+    //setting the input 
     public void setInput (String input){
         this.input = input;
     }
 
+    //movements
     public void move(String input){
-
+	
+	//if the user goes up
         if(input.equals("up")) {
+	    //updated coordinates
             this.setPosition(this.x, this.y - 1);
             
         }
+	//if the user goes right
         else if(input.equals("right")){
+	    //updated coordinates
             this.setPosition(this.x+1,this.y);
             
         }
+	//if the user goes left
         else if (input.equals("left")){
+	    //updated coordinates
             this.setPosition(this.x-1, this.y);
 
         }
     }
     
 
+    //displays resulting coordinat
     public String toString(){
         return new String(this.name + " is at " + this.x+","+this.y);
     }
 
+	//setters and getters
 	public int getX() {
 		return x;
 	}
