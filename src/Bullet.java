57259e310db1e6c808cf6dba0da173494df37f52
Razeenadil <60667310+Razeenadil@users.noreply.github.com>
2020-02-26 13:38:57 -0700

    Update Bullet.java

--- src/Bullet.java
+++ src/Bullet.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 //Shooting the bullet
 //Desmond and Muhammad and Tuan
 public class Bullet {
+	//private variables 
 	private static final int WIDTH = 400;
 	private int bx;
 	private int by;
 	private String space;
+	//array list to store bullets fired
 	private ArrayList<Integer>bulletList;
 	
 	
@@ -15,6 +17,7 @@ public class Bullet {
 		this.bulletList = new ArrayList<Integer>();
 	}
 	
+	//sets the inital position of the bullet 
 	public void setBullet(int bx, int by) {
 		this.bx = bx;
 		this.by = by;
@@ -25,18 +28,24 @@ public class Bullet {
 	//updating x and y depending on where the player is
 	public void shoot(String space, int bx, int by) {
 		ArrayList<Integer>bullets = new ArrayList<Integer>();
-
+		
+		//adding original bullet position to the array
 		this.bulletList.add(this.bx);
 		this.bulletList.add(this.by);
+		
+		//printing the bullet coordinates
 		System.out.println(this.bx + this.by);
 		
+		//if the user shoots a bullet
 		if (this.space.equals("shoot")) {
 			int newX = 0;
+			//loops thru the bullet array
 			for(int bullet : this.bulletList) {
+				//updates the coordinates of the bullets, newX is the new coordinate of the bullet
 				for(int bulletx = 0; bulletx < bulletList.size(); bulletx++) {
 					newX = this.bx+bulletx;
 				}
-		
+			//adding new coordinates to the array
 			bullets.add(newX,this.by);
 				}
 			
@@ -68,8 +77,5 @@ public class Bullet {
 	public void setBulletList(ArrayList<Integer> bulletList) {
 		this.bulletList = bulletList;
 	}
-	
-	
-
 }
 
