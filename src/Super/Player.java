ea1aeb96d585e10d0647a3c16b88ecec96d19360
Anh Tuan Pham <55847291+pooniena2@users.noreply.github.com>
2020-03-12 15:50:41 -0600

    Update Player.java
    Update comments for demo 2

--- src/Super/Player.java
+++ src/Super/Player.java
@@ -9,24 +9,35 @@ import java.awt.Graphics;
 import game.Handler;
 import game.ID;
 
+/* Class player
+*This class represents the movement of the player
+* This class uses these following libraries abcd 
+*/
+
 public class Player extends Super {
 
 	public Player(int x, int y, int WITDH, int HEIGHT, boolean s, ID id, Handler handler) {
 		super(x, y, WITDH, HEIGHT, s, id, handler);
 	}
-
+	/*This method update the movement of the player and create the bounce to restrict the player not to get out of the screen.
+	*This also check the collision between players and evironment by looping through the environment data inside the object linkedlist
+	and check the ID if it is a wall or not. If it is a wall, it's going to get the intersection and avoid player to jump through it.
+	*Then it also implements the gravity of the player. Using boolean for jumping and falling to check the position of the player.
+	* If the player is jumping, set the update gravity to -0.2, else set the update gravity to +0.1.
+	*/
+	@Override
 	public void update() {
 		x += velocityX;
 		y += velocityY;
 		if (x <= 0)
 			x = 0;
-		// if (y <=0) y=0;
+
 		if (x + WIDTH >= 1080)
 			x = 1080 - WIDTH;
 		if (y + HEIGHT >= 771)
 			y = 771 - HEIGHT;
 
-		// COLLISION DETECTION
+
 		for (Tile enviro : handler.object) {
 			if (enviro.s == false)
 				break;
@@ -65,7 +76,7 @@ public class Player extends Super {
 			}
 		}
 
-		// GRAVITY
+		// Gravity
 		if (jumping == true) {
 			gravity -= 0.2;
 			setVelocityY((int) -gravity);
@@ -79,14 +90,13 @@ public class Player extends Super {
 			gravity += 0.1;
 
 			setVelocityY((int) gravity);
-//			System.out.println(gravity + " dsadagdwigdiawdwa");
-//			if (gravity == 1) {
-//				death();
+
 			
 		}
 
 	}
-
+	/*this method is used to create a box that represents the player before impletment the image
+	*/
 	public void render(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(x, y, WIDTH, HEIGHT);
