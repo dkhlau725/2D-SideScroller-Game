f91f9600fa6aa860c66f322205bc8da624a0a28d
Anh Tuan Pham <55847291+pooniena2@users.noreply.github.com>
2020-03-11 22:59:21 -0600

    Update Super.java
    fixed privacy leaks

--- src/Super/Super.java
+++ src/Super/Super.java
@@ -10,7 +10,7 @@ public abstract class Super {
 	public int x;
 	public int y;
 	public int WIDTH;
-	public int HEIGHT;	
+	public int HEIGHT;
 	public int velocityX;
 	public int velocityY;
 	public ID id;
@@ -20,30 +20,21 @@ public abstract class Super {
 	public boolean s = false;
 	public boolean falling = true;
 	public double gravity = 0.0;
-	
-	public Super(int x , int y, int WIDTH, int HEIGHT, boolean s, ID id, Handler handler) {
-		this.x = x;
-		this.y = y;
-		this.WIDTH = WIDTH;
-		this.HEIGHT = HEIGHT;
-		this.s = s;
-		this.id = id;
-		this.handler = handler;
-		//this.facing = facing;
+
+	public Super(int x, int y, int WIDTH, int HEIGHT, boolean s, ID id, Handler handler) {
+		setX(x);
+		setY(y);
+		setWIDTH(WIDTH);
+		setHEIGHT(HEIGHT);
+		setS(s);
+		setId(id);
+		setHandler(handler);
+		
 	}
 
-	
-
 	public abstract void update();
-	
+
 	public abstract void render(Graphics g);
-	
-	
-	/*
-	 * public int getFacing() { return facing; }
-	 * 
-	 * public void setFacing(int facing) { this.facing = facing; }
-	 */
 	
 	public int getX() {
 		return x;
@@ -60,80 +51,78 @@ public abstract class Super {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
-
-	
-
-	public void death() {
-		handler.removeSuper(this);
-		
-	}
-
-	public void setVelocityX(int velocityX) {
-		this.velocityX = velocityX;
-	}
-
-	public ID getId() {
-		return id;
-	}
-
-	public void setVelocityY(int velocityY) {
-		this.velocityY = velocityY;
-	}
-
-	public Rectangle getBounds(){
-		return new Rectangle(getX(), getY(), WIDTH, HEIGHT);
-	}
-
-	public Rectangle getBoundsUp() {
-		return new Rectangle(getX()+10, getY(), WIDTH-20, 5);
-	}
-	
-	public Rectangle getBoundsDown() {
-		return new Rectangle(getX()+10, getY()+HEIGHT-5, WIDTH-20, 5);
-	}
-	
-	public Rectangle getBoundsLeft() {
-		return new Rectangle(getX(), getY()+10, 5, HEIGHT-20);
-	}
-	
-	public Rectangle getBoundsRight() {
-		return new Rectangle(getX()+WIDTH-5, getY()+10, 5, HEIGHT-20);
-	}
-
-
 
 	public int getWIDTH() {
 		return WIDTH;
 	}
 
-
-
 	public void setWIDTH(int wIDTH) {
 		WIDTH = wIDTH;
 	}
-
-
 
 	public int getHEIGHT() {
 		return HEIGHT;
 	}
 
-
-
 	public void setHEIGHT(int hEIGHT) {
 		HEIGHT = hEIGHT;
 	}
-
-
 
 	public boolean isS() {
 		return s;
 	}
 
-
-
 	public void setS(boolean s) {
 		this.s = s;
 	}
+	
+	public ID getId() {		
+		return id;
+	}
+	
+	public void setId(ID i) {
+		this.id = i;
+	}
+	
+	public void setHandler(Handler h) {
+		this.handler = h;
+	}
+	
+	public Handler getHandler() {
+		return new Handler();
+	}
+	
+	public void death() {
+		handler.removeSuper(this);
+	}
+
+	public void setVelocityX(int velocityX) {
+		this.velocityX = velocityX;
+	}
+	
+	public void setVelocityY(int velocityY) {
+		this.velocityY = velocityY;
+	}
+
+	public Rectangle getBounds() {
+		return new Rectangle(getX(), getY(), WIDTH, HEIGHT);
+	}
+
+	public Rectangle getBoundsUp() {
+		return new Rectangle(getX() + 10, getY(), WIDTH - 20, 5);
+	}
+
+	public Rectangle getBoundsDown() {
+		return new Rectangle(getX() + 10, getY() + HEIGHT - 5, WIDTH - 20, 5);
+	}
+
+	public Rectangle getBoundsLeft() {
+		return new Rectangle(getX(), getY() + 10, 5, HEIGHT - 20);
+	}
+
+	public Rectangle getBoundsRight() {
+		return new Rectangle(getX() + WIDTH - 5, getY() + 10, 5, HEIGHT - 20);
+	}
+
+
 }
