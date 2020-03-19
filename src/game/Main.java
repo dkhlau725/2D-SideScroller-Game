16c36cedf2bb8821661344ebb5c931cf77876da9
dkhlau725 <60667307+dkhlau725@users.noreply.github.com>
2020-03-18 22:23:18 -0600

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,159 +1,115 @@
 package game;
 
-import java.awt.Canvas; // allow  to display with our frame
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
 
-import javax.swing.JFrame; //library to create a window :) 
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.geometry.Dimension2D;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import Super.Enemy0;
 import Super.Player;
 import Super.Super;
 import key.KeyboardInput;
 
-public class Main extends Canvas implements Runnable {
-	// set screen resolution as 800x600 and the scale as 3 :) name of the game will
-	// be decided later so let set it random
+public class Main extends Application {
+	
 	public static final int WIDTH = 270;
 	public static final int HEIGHT = WIDTH / 14 * 10;
 	public static final int SCALE = 4;
-	public static final String name = "Scrolling Shooting Game"; // title of the game
-	private Thread thread;
-	private boolean rungame = false; // boolean to run game the game
-	public static Handler handler;
-	public static Camera cam;
-	public static KeyboardInput key;
+	public Handler handler;
+	public KeyboardInput key;
+	public Player player;
+	public Canvas canvas;
+	public Enemy0 enemy0;
+	
+	 public void start(Stage stage) {
+		  try {	        	
+			  Group root = new Group();
+		      Scene scene = new Scene(root, frameWidth(), frameHeight(), Color.BLACK);
+		      this.canvas = new Canvas(frameWidth(), frameHeight());
+		      stage.setScene(scene);
+		      root.getChildren().add(canvas);
+		      
+		      handler = new Handler();
+		      
+		      this.player = new Player(300, 615, 64, 64, true, ID.player, handler);
+		      handler.addSuper(player);
+		      
+		      this.enemy0 = new Enemy0(500, 615, 55, 55, true, ID.Enemy0, handler);
+		      handler.addEnemy0(enemy0);
+		      
+		      key = new KeyboardInput();
+		      canvas.getScene().setOnKeyPressed(event -> key.keyPressed(event, handler));
+		      canvas.getScene().setOnKeyReleased(event -> key.keyReleased(event, handler));
+		      
+		      // create new timeline 
+		      //add keyframe of 20ms
+		        Timeline gameLoop = new Timeline();
+		        gameLoop.setCycleCount(Timeline.INDEFINITE);
+		        if(handler.player.size() == 0) {
+		        	  System.out.println("tuan");
+			    	  gameLoop.stop();
+			      }
 
-	// start the game
-	private synchronized void start() {
-		System.out.println("the game has been started");
-		if (rungame)
-			return;
-		rungame = true;
-		thread = new Thread(this, "Thread");
-		thread.start();
-	}
-
-	// end the game
-	private synchronized void end() {
-		if (rungame = false) {
-			return;
+		      
+		        KeyFrame frame = new KeyFrame(new Duration(20), (event) -> this.render());
+		        gameLoop.getKeyFrames().add(frame);
+		        gameLoop.play();
+			  
+		      
+		      stage.setTitle("scrolling shooting game");
+		      stage.show();
+		      
+		      
+		  }
+		  catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	 }
+	 
+	 public static int frameWidth() {
+			return WIDTH * SCALE;
 		}
-		// using try and catch to get error when closing the game
-		try {
-			thread.join();
-		} catch (InterruptedException error) {
-			error.printStackTrace();
-		}
-	}
-
-	// RUN THE GAME
-	public void run() {
-		init();
-		requestFocus();
-		System.out.println("the game is running");
-		long previoustime = System.nanoTime(); // begin time in nanosec
-		long time = System.currentTimeMillis();// current time
-		double delta = 0.0; // different time to update
-		int frame = 0; // counter to update background
-		int update = 0;
-		double ns = 1000000000.0 / 60.0;
-		int counter = 0; // counter for update
-
-		// if the game is running, update activities of the game and background
-		while (rungame = true) {
-			long currenttime = System.nanoTime();
-			delta = delta + (currenttime - previoustime) / (ns);
-			previoustime = currenttime;
-			while (delta >= 1) {
-				update();
-				counter++;
-				delta--;
-			}
-			render();
-			frame++;
-			if (System.currentTimeMillis() - time > 1000) {
-				time += 1000;
-				System.out.println(frame + " Frame per s " + counter + " update per s");
-				frame = 0;
-				counter = 0;
-			}
-		}
-		end();
-	}
-
-	// initialize a box to be our character (temporary)
-	private void init() {
-		handler = new Handler();
-		handler.addSuper(new Player(300, 615, 64, 64, true, ID.player, handler));
-		cam = new Camera();
-		key = new KeyboardInput();
-		addKeyListener(key);
-
-		// makes a box to be an enemy (temporary)
-		handler.addSuper(new Enemy0(500, 615, 55, 55, true, ID.Enemy0, handler));
-
-	}
-
-	// update everything from handler function
-	public void update() {
-		handler.update();
-
-		for (Super character : handler.player) {
-			if (character.getId() == ID.player) {
-				cam.updateCam(character);
-			}
-		}
-	}
-
-	// update background and graphic
-	public void render() {
-		BufferStrategy background = getBufferStrategy();
-		if (background == null) {
-			createBufferStrategy(3);
-			return;
-		}
-
-		Graphics g = background.getDrawGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.translate(cam.getX(), cam.getY());
-		handler.render(g);
-		g.dispose();
-		background.show();
-	}
-
-	public static int frameWidth() {
-		return WIDTH * SCALE;
-	}
 
 	public static int frameHeight() {
-		return HEIGHT * SCALE;
+			return HEIGHT * SCALE;
+		}
+
+	public void render() {
+		GraphicsContext context = this.canvas.getGraphicsContext2D();
+		context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		if (handler.player.size() != 0) {
+		for (int i = 0; i < handler.player.size(); i++) {
+			Super play = handler.player.get(i);
+			context.setFill(Color.RED);
+			context.fillRect(play.getX(), play.getY(), 64, 64);	
+			update();
+			}
+		}
+		
+		for (int e = 0; e < handler.enemy0.size(); e++) {
+			Enemy0 en = handler.enemy0.get(e);
+			context.setFill(Color.GREEN);
+			context.fillRect(en.getEnemyX(), en.getEnemyY(), 64, 64);
+			update();   	
+			}
 	}
-
-	// create dimension which makes max width increase to 2400 and max height
-	// increase to 1800 :)
-	public Main() {
-		Dimension size = new Dimension(frameWidth(), frameHeight());
-		setPreferredSize(size);
-		setMaximumSize(size);
-		setMinimumSize(size);
+	
+	
+	public void update() {
+        this.handler.update();
 	}
-
-	// main function to start/exit the window on click
-	public static void main(String args[]) {
-		Main game = new Main();
-		JFrame frame = new JFrame(name);
-		frame.add(game);
-		frame.pack();
-		frame.setResizable(false); // can't change the default game window
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		game.start();
-
-	}
-
+	
+	 public static void main(String args[]) {
+		 launch(args);
+	 }
 }
