26a74b10a7919a747d555b2d74deb03ccb4fbc62
dkhlau725 <60667307+dkhlau725@users.noreply.github.com>
2020-03-31 19:44:28 -0600

    Update ID.java

--- src/game/ID.java
+++ src/game/ID.java
@@ -1,10 +1,6 @@
 package game;
-/*Desmond
-*/
-/*Enum ID
-*this class used to check weather if it's player, wall, enemy, or bullet.
-*/
+
 public enum ID {
-	player, wall, Enemy0, bullet;
+	player, tile, Enemy, bullet, spikes;	
 
 }
