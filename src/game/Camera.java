5198663b1381adae19adbf713f10b646fefc83fa
dkhlau725 <60667307+dkhlau725@users.noreply.github.com>
2020-03-31 19:46:07 -0600

    Update Camera.java

--- src/game/Camera.java
+++ src/game/Camera.java
@@ -1,17 +1,14 @@
 package game;
-/* Desmond
-*/
+
 import Super.Super;
-/*Class Camera
-* This class used to create a camera that follows the player while it moves around the platform. Since we only implement the game
-*vertically so we only update variable Y. 
-*/
+
 public class Camera {
 
 	public int x;
 	public int y;
 	
 	public void updateCam(Super player) {
+		
 		setY(-player.getY() + Main.HEIGHT*2);
 	}
 	
@@ -30,4 +27,3 @@ public class Camera {
 
 	
 }
-//
